82b1de2545f53a29bee98896833ffbad16d1f766
x1nchen <xinchenccx@gmail.com>
2021-07-06 00:05:28 +0800

    dp: improved Fibonacci with memorization

--- dynamic-programming/p03/main.go
+++ dynamic-programming/p03/main.go
@@ -2,6 +2,7 @@ package main
 
 func main() {
 	println(Fibonacci(10))
+	println(FibonacciAdvance(10, make([]int, 10+1)))
 }
 
 func Fibonacci(i int) int {
@@ -11,6 +12,19 @@ func Fibonacci(i int) int {
 
 	if i > 1 {
 		return Fibonacci(i-1) + Fibonacci(i-2)
+	}
+
+	return 0
+}
+
+func FibonacciAdvance(i int, memo []int) int {
+	if i == 0 || i == 1 {
+		return i
+	}
+
+	if i > 1 {
+		memo[i] = Fibonacci(i-1) + Fibonacci(i-2)
+		return memo[i]
 	}
 
 	return 0
